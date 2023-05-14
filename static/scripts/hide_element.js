d05144eaccf46ce7600d4d76137c48c9543e1c97
codeasarjun <93047517+codeasarjun@users.noreply.github.com>
2023-05-14 11:31:44 +0530

    script for hiding is working

---
+++ static/scripts/hide_element.js
@@ -0,0 +1,15 @@
+function get_price(){
+    // alert("pricing is getting displayed")
+   let el=document.getElementById("show_result");
+
+   if(el.style.display=="none"){
+    el.style.display="block";
+
+   }else{
+    el.style.display="none";
+   }
+   
+   alert("hello");
+    
+ 
+ }
